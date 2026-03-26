e1e8483cf8e7320a91c75c9414351fcf7342578b
codewithnill <nbnill.hmlnmh@gmail.com>
2026-03-26 15:53:24 +0600

    chapter 1 : variable data types

---
+++ JAVASCRIPT/Chapter 1-Variables Data Types/first.js
@@ -0,0 +1,19 @@
+console.log("Apna College...");
+console.log("Shradha Khapra");
+console.log("I love JS");
+fullname="Tony stark";
+console.log(fullname);
+
+console.log("Data types :");
+console.log("Primitive :");
+let age=24;
+let x=BigInt(3);
+let y=Symbol("Hello!");
+
+console.log("Non Primitive :");
+const student = {
+    fullname : "Nb",
+    age : 49, 
+    cgpa : 2.50,
+    isPass : false
+};
---
+++ JAVASCRIPT/Chapter 1-Variables Data Types/practicequestion.js
@@ -0,0 +1,8 @@
+const product = {
+    name: "Ballpen",
+    rating: 5,
+    price: 199.00,
+    offer: 5
+}
+
+console.log(product);
