6432b959405721c7649373dc0963471f0d8414ae
codewithnill <nbnill.hmlnmh@gmail.com>
2026-04-10 21:37:47 +0600

    Practice questions

---
+++ JAVASCRIPT/Chapter 7-DOM part 2/practice_questions/script.js
@@ -0,0 +1,8 @@
+let btn=document.createElement("button");
+btn.innerText="click me!";
+btn.style.backgroundColor="red";
+btn.style.color="white";
+document.querySelector("body").prepend(btn);
+
+
+let para=document.querySelector("p");
